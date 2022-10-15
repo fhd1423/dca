fa8271374efddf0286847814c77c9aa48b711db6
fhd <rohithvenkatesan142@gmail.com>
2022-10-15 16:46:20 -0400

    landing page

--- dca-next-app/pages/index.js
+++ dca-next-app/pages/index.js
@@ -3,7 +3,13 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='font-extrabold'> hii
+    <div className=' relative bg-black h-screen w-screen'>
+      <div className='w-full absolute top-40 text-center text-2xl font-bold text-gray-400'>
+        Dollar Cost Average into Bitcoin, Ethereum, and more...🚀
+        <div className='animate-bounce w-full font-bold text-gray-400'>
+          without the <span className='text-white'> fees, </span> or the <span className='text-white'> hassle.🌟 </span>
+        </div>
+      </div>
     </div>
   )
 }
