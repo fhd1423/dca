0f82e2c6d2d35c9f1939e71a6689d73358a46b3c
Yunki <kmj9ab@virginia.edu>
2022-10-15 14:49:56 -0400

    test1

--- dca-next-app/pages/index.js
+++ dca-next-app/pages/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='font-extrabold'> hii
+    <div className='font-extrabold'> hii boy
     </div>
   )
 }
