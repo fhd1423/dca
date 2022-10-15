7a9bf7ca936b3961d3aa9465f3bcf297f41738d2
fhd <rohithvenkatesan142@gmail.com>
2022-10-15 19:56:13 -0400

    logout button

--- dca-next-app/pages/account.js
+++ dca-next-app/pages/account.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { useSession, signIn, signOut, getSession } from 'next-auth/react'
 
 const account = () => {
-    const options=[
-        {label: 'daily', value: 'daily'},
-        {label: 'weekly', value: 'weekly'},
-        {label: 'monthly', value: 'monthly'},
+    const options = [
+        { label: 'daily', value: 'daily' },
+        { label: 'weekly', value: 'weekly' },
+        { label: 'monthly', value: 'monthly' },
     ];
 
     const [value, setValue] = React.useState('daily');
@@ -13,45 +14,62 @@ const account = () => {
         setValue(event.target.value);
     };
 
-    function submit(e){
+    function submit(e) {
         e.preventDefault();
         alert(apiKey);
     }
-    
-  return (
-    <div className=' relative bg-black h-screen w-screen'>
-      <div className='w-full absolute top-40 text-center text-2xl f ont-bold text-gray-400'>
-        Connect to Binance
-        <div className='w-full font-bold text-gray-400'>
-            API Key:
-            <input type="text" name="apiKey"/>
-            <div>
-                API Secreet:
-                <input type="text" name="apiSecret"/>
-            </div>
-            <div>
-                Frequency of purchase:
-                <select value={value} onChange={handleChange}>
-                    {options.map((option)=>(
-                        <option value={option.value}>{option.label}</option>
-                    ))}
-                </select>
-            </div>
-            <div>
-                amount:
-                <input type="text" name="amount"/>
 
-                <div>
-                    <button onClick={submit}>
-                        Submit
-                    </button>
+    return (
+        <div className=' relative bg-black h-screen w-screen'>
+            <button onClick={() => signOut()} className='text-gray-400 absolute right-0 mt-10 mr-10 h-18 w-36 p-4 border-white bg-white rounded-lg hover:text-black'>
+                Logout
+            </button>
+            <div className='w-full absolute top-40 text-center text-2xl f ont-bold text-gray-400'>
+                Connect to Binance
+                <div className='w-full font-bold text-gray-400'>
+                    API Key:
+                    <input type="text" name="apiKey" />
+                    <div>
+                        API Secreet:
+                        <input type="text" name="apiSecret" />
+                    </div>
+                    <div>
+                        Frequency of purchase:
+                        <select value={value} onChange={handleChange}>
+                            {options.map((option) => (
+                                <option value={option.value}>{option.label}</option>
+                            ))}
+                        </select>
+                    </div>
+                    <div>
+                        amount:
+                        <input type="text" name="amount" />
+
+                        <div>
+                            <button onClick={submit}>
+                                Submit
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
+export const getServerSideProps = async (context) => {
+    const session = await getSession(context)
+    console.log(session)
+    if (!session) {
+        return {
+            redirect: {
+                destination: '/'
+            }
+        }
+    }
+    return {
+        props: { session },
+    }
+}
 
 
 export default account
